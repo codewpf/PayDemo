e45e6ee5fe2451c908d52f20fd85aa4dd6a8ed42
ioswpf <ioswpf@gmail.com>
2015-12-30 20:37:35 +0800

    Initial Commit

---
+++ PayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayDemo
+//
+//  Created by wpf on 15/12/30.
+//  Copyright © 2015年 wpf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayDemo
+//
+//  Created by wpf on 15/12/30.
+//  Copyright © 2015年 wpf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
